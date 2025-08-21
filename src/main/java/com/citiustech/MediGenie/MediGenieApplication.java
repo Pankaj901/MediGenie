cd11b0a9731b6291e8b1716767b0bfe16c1c7e33
pankaj kumar pal <pankajpal901@gmail.com>
2025-08-21 14:32:11 +0530

    spring init code

---
+++ src/main/java/com/citiustech/MediGenie/MediGenieApplication.java
@@ -0,0 +1,13 @@
+package com.citiustech.MediGenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediGenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediGenieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citiustech/MediGenie/MediGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citiustech.MediGenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediGenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
